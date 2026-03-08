2346502a510c703435646447efbfcaa884d54530
sam <sam.newman98@gmail.com>
2026-03-08 20:13:25 +0000

    Add deploy.yml. Remove unused api key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,13 +28,6 @@ export default defineNuxtConfig({
     host: '0.0.0.0',
   },
 
-  runtimeConfig: {
-    public:
-    {
-      geoapifyApiKey: process.env.NUXT_PUBLIC_GEOAPIFY_API_KEY || '',
-    },
-  },
-
   ssr: false,
 
 })
